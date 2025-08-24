acfd71d839aedcd004de29f0328ccb1de5102e09
luxtortrab25-jc <luxtortrab25@gmail.com>
2025-08-23 21:44:19 -0600

    Update social-bar.tsx
    Actualización del enlace de TikTok

--- src/components/layout/social-bar.tsx
+++ src/components/layout/social-bar.tsx
@@ -46,7 +46,7 @@ const socialLinks = [
   },
   {
     name: 'TikTok',
-    href: 'https://www.tiktok.com/@luxtor.detail.stu?_t=ZS-8y3NcC83Z6V&_r=1',
+    href: 'https://www.tiktok.com/@luxtordetailstudio?_t=ZS-8z7OJ7XFiGA&_r=1',
     icon: (
       <Image
         src="https://i.postimg.cc/NMYR0KQC/tiktok-icono-32.png"
