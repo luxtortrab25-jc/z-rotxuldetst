e936b738a9a2c76e44e8322c217377adb713a1fa
luxtortrab25-jc <luxtortrab25@gmail.com>
2025-09-11 20:37:46 -0600

    Update footer.tsx
    f

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,10 +5,6 @@ export default function Footer() {
         <p className="text-center text-sm text-muted-foreground">
           © {new Date().getFullYear()} Luxtor Detail Studios. Todos los derechos reservados.
         </p>
-      <div className="container flex h-24 flex-col items-center justify-center py-4">
-        <p className="text-center text-sm text-muted-foreground">
-          © {new Date().getFullYear()}  Este sitio web pertenece a Raquel Juárez Ugalde.
-        </p>
       </div>
     </footer>
   );
